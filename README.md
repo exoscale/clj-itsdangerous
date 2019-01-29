3c89a0751dbe48cd55598fe13ddc9f7c016a9c3d
Yoan Blanc <yoan.blanc@exoscale.ch>
2019-01-29 10:42:12 +0100

    readme: add badge
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 A Clojure library designed to sign/unsign [itsdangerous][] payloads.
 
 [![Build Status](https://travis-ci.com/exoscale/clj-itsdangerous.svg?branch=master)](https://travis-ci.com/exoscale/clj-itsdangerous)
+[![Clojars Project](https://img.shields.io/clojars/v/exoscale/itsdangerous.svg)](https://clojars.org/exoscale/itsdangerous)
 
 ## Usage
 
--- project.clj
+++ project.clj
@@ -7,8 +7,6 @@
                  [org.clojure/test.check "0.9.0" :scope "test"]
                  [buddy/buddy-core "1.5.0"]
                  [buddy/buddy-sign "3.0.0"]]
-  :plugins [[exoscale/sos-wagon-private "1.3.2-exoscale8"]]
   :codox {:namespaces [#"itsdangerous"]}
-  :repositories [["private" {:url "s3p://exo-artifacts/releases" :no-auth true :sign-releases false}]]
   :profiles {:dev {:plugins [[lein-codox "0.10.5"]]
              :global-vars {*warn-on-reflection* true}}})
