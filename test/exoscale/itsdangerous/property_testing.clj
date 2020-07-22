4f8cdfc6abe61be6150933aea817135d683c2f1a
Pierre-Yves Ritschard <pyr@spootnik.org>
2020-07-22 10:55:05 +0200

    Support key rotation
    To allow key rotation over time, introduce a new config key
    `::private-keys`, a collection of keys to try building signatures
    with.
    
    The first key in the collection is always selected for signing,
    but verifying tries all keys, this allows adding keys over time.
    
    `sign` and `verify` now allow additional arities for the payload
    and timestamp (or max-age).

--- test/exoscale/itsdangerous/property_testing.clj
+++ test/exoscale/itsdangerous/property_testing.clj
@@ -11,76 +11,63 @@
 (stest/instrument `danger/verify)
 (stest/instrument `danger/parse-token)
 (stest/instrument `danger/signature-for)
+(stest/instrument `danger/main-key)
 
 (defspec roundtrip-sign-to-verify
   10000
   (prop/for-all
-   [input (s/gen (s/keys :req [::danger/algorithm ::danger/payload
-                               ::danger/private-key ::danger/salt]))]
-   (let [token (danger/sign input)]
-     (= (::danger/payload input)
-        (danger/verify (-> input
-                           (dissoc ::danger/payload)
-                           (assoc ::danger/token token)))))))
+   [config (s/gen ::danger/config)
+    payload (s/gen ::danger/payload)]
+   (let [token (danger/sign config payload)]
+     (= payload (danger/verify config token)))))
 
 (defspec token-validity-is-enforced
   10000
   (prop/for-all
-   [input (s/gen (s/keys :req [::danger/algorithm ::danger/payload
-                               ::danger/private-key ::danger/salt]))]
-   (let [token (danger/sign (assoc input ::danger/timestamp 0))]
+   [config  (s/gen ::danger/config)
+    payload (s/gen ::danger/payload)]
+   (let [token (danger/sign config payload 0)]
      (= [:exoscale.ex/forbidden "token validity expired"]
-        (-> (try
-              (danger/verify (-> input
-                                 (dissoc ::danger/payload)
-                                 (assoc ::danger/token token)
-                                 (assoc ::danger/max-age 86400)))
-              (catch Exception e
-                [(:type (ex-data e)) (ex-message e)])))))))
+        (try
+          (danger/verify config token 86400)
+          (catch Exception e
+            [(:type (ex-data e)) (ex-message e)]))))))
 
 (defspec token-validity-is-enforced-small-window
   10000
   (prop/for-all
-   [input (s/gen (s/keys :req [::danger/algorithm ::danger/payload
-                               ::danger/private-key ::danger/salt]))]
-   (let [token (danger/sign
-                (-> input
-                    (assoc ::danger/timestamp (dec (danger/epoch)))))]
+   [config  (s/gen ::danger/config)
+    payload (s/gen ::danger/payload)]
+   (let [token (danger/sign config payload (dec (danger/epoch)))]
      (= [:exoscale.ex/forbidden "token validity expired"]
-        (-> (try
-              (danger/verify (-> input
-                                 (dissoc ::danger/payload)
-                                 (assoc ::danger/token token)
-                                 (assoc ::danger/max-age 0)))
-              (catch Exception e
-                [(:type (ex-data e)) (ex-message e)])))))))
+        (try
+          (danger/verify config token 0)
+          (catch Exception e
+            [(:type (ex-data e)) (ex-message e)]))))))
 
 (defspec token-signature-is-enforced-private-key-variant
   10000
   (prop/for-all
-   [input (s/gen (s/keys :req [::danger/algorithm ::danger/payload
-                               ::danger/private-key ::danger/salt]))]
-   (let [token (danger/sign input)]
+   [base        (s/gen ::danger/config)
+    private-key (s/gen ::danger/private-key)
+    payload     (s/gen ::danger/payload)]
+   (let [good-config (assoc base ::danger/private-keys [private-key])
+         bad-config  (assoc base ::danger/private-keys [(str private-key "f")])
+         token       (danger/sign good-config payload)]
      (= [:exoscale.ex/forbidden "invalid signature"]
-        (-> (try
-              (danger/verify (-> input
-                                 (dissoc ::danger/payload)
-                                 (assoc ::danger/token token)
-                                 (update ::danger/private-key str "suffix")))
-              (catch Exception e
-                [(:type (ex-data e)) (ex-message e)])))))))
+        (try
+          (danger/verify  bad-config token)
+          (catch Exception e
+            [(:type (ex-data e)) (ex-message e)]))))))
 
 (defspec token-signature-is-enforced-salt-variant
   10000
   (prop/for-all
-   [input (s/gen (s/keys :req [::danger/algorithm ::danger/payload
-                               ::danger/private-key ::danger/salt]))]
-   (let [token (danger/sign input)]
+   [config  (s/gen ::danger/config)
+    payload (s/gen ::danger/payload)]
+   (let [token (danger/sign config payload)]
      (= [:exoscale.ex/forbidden "invalid signature"]
-        (-> (try
-              (danger/verify (-> input
-                                 (dissoc ::danger/payload)
-                                 (assoc ::danger/token token)
-                                 (update ::danger/salt str "suffix")))
-              (catch Exception e
-                [(:type (ex-data e)) (ex-message e)])))))))
+        (try
+          (danger/verify (update config ::danger/salt str "suffix") token)
+          (catch Exception e
+            [(:type (ex-data e)) (ex-message e)]))))))
